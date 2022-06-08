c8bae7e1cd72f578bff997cadfe27fdd9d0f299d
yaser <ddemety372@gmail.com>
2022-06-08 23:11:38 +0300

    list properties

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 setTimeout(() => {
   window.location.reload();
-}
-  , 40000);
-
+}, 40000);
 
 const audio2 = document.getElementById("audio2");
 const audio = document.getElementById("audio");
 
-
-
-
-
-
-
-
-
+const section2 = document.querySelector(".second-sec");
 const section1 = document.querySelector(".first-section");
 const button = document.querySelector(".add");
 const input = document.querySelector(".form-control");
@@ -28,9 +19,6 @@ section1.appendChild(ul);
 input.focus();
 
 function ekle() {
-
-
-
   if (input.value == "") {
     alert("Please add a task ...");
   } else if (ul.childElementCount > 7) {
@@ -51,146 +39,99 @@ function ekle() {
   }
 }
 
-
+// ? ADD TASK
 
 button.addEventListener("click", ekle);
 
 input.addEventListener("keydown", (e) => {
-  if (e.code == "Enter") {
+  if (e.keyCode == 13) {
     ekle();
+    audio.play();
+    audio.currentTime = 0;
   }
 });
 
-
-
-
-
-
 function deleteAll() {
-
   if (ul.childElementCount > 0) {
-
     ul.removeChild(ul.lastElementChild);
-
 
     if (ul.childElementCount > 1) {
       task.innerHTML = `You have ${ul.childElementCount} tasks`;
-    }
-    else if (ul.childElementCount == 0) {
+    } else if (ul.childElementCount == 0) {
       task.innerHTML = `You have no tasks`;
-    }
-    else if (ul.childElementCount == 1) {
+    } else if (ul.childElementCount == 1) {
       task.innerHTML = `You have only a task`;
     }
-
-
-
-
   } else {
     alert("There is no task to delete");
   }
-
-
-
 }
-
-
-
 
 deleteBtn.addEventListener("click", deleteAll);
 
-ul.addEventListener("click", event => {
-
-
+ul.addEventListener("click", (event) => {
+  audio2.play();
+  audio.currentTime = 0;
 
   if (event.target.classList.contains("x")) {
-
-
     if (ul.childElementCount > 1) {
       task.innerHTML = `You have ${ul.childElementCount} tasks`;
-    }
-    else if (ul.childElementCount == 0) {
+    } else if (ul.childElementCount == 0) {
       task.innerHTML = `You have no tasks`;
-    }
-    else if (ul.childElementCount == 1) {
+    } else if (ul.childElementCount == 1) {
       task.innerHTML = `You have only a task`;
     }
 
-
-
-
-
-
-    event.target.parentElement.classList.add("animation")
+    event.target.parentElement.classList.add("animation");
 
     setTimeout(() => {
-      event.target.parentElement.remove()
-
-    }, 2000)
+      event.target.parentElement.remove();
+    }, 2000);
   }
+});
 
-
-
-})
-
-
-
-
-ul.addEventListener("click", event => {
-
+ul.addEventListener("click", (event) => {
   if (event.target.classList.contains("fa-circle-check")) {
-
-    event.target.parentElement.innerHTML = "TASK COMPLETED !  DO YOU WANT TO DELETE IT PERMANENTLY ? <i class='fa-solid x fa-trash'></i>"
-    event.target.parentElement.classList.add("liste")
-
-
+    event.target.parentElement.innerHTML =
+      "TASK COMPLETED !  DO YOU WANT TO DELETE IT PERMANENTLY ? <i class='fa-solid x fa-trash'></i>";
+    event.target.parentElement.classList.add("liste");
   }
-
-})
+});
 
 const clock = document.querySelector(".clock");
 
 clock.innerHTML = `${new Date().toLocaleTimeString()}`;
 
-
 const date = document.querySelector(".date");
 date.innerHTML = `${new Date().toLocaleDateString()}`;
 
-
-
 // ? PART OF AUDIO
-
 
 deleteBtn.addEventListener("click", () => {
   audio2.play();
-})
+  audio.currentTime = 0;
+});
 
+// input.addEventListener("keydown", (e) => {
+//   if(e.keyCode == 46){
+//     deleteAll();
+//   }
+// })
 
 button.addEventListener("click", () => {
   audio.play();
-
 });
-
-
-
-
-
 
 // ! PART OF LOCK
 
 const locked = document.querySelector(".fa-lock");
 
-
 button.addEventListener("click", () => {
-
-  locked.setAttribute("class", "fa-solid fa-unlock-keyhole")
-
-})
-
+  locked.setAttribute("class", "fa-solid fa-unlock-keyhole");
+});
 
 deleteBtn.addEventListener("click", () => {
-
   if (ul.childElementCount == 0) {
-    locked.setAttribute("class", "fa-solid fa-lock")
+    locked.setAttribute("class", "fa-solid fa-lock");
   }
-})
+});
